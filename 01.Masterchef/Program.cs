fc8ddf96c786aea1f55a7c734cea664a4064e9a2
mariaIvanova-vn <maria_vn86@abv.bg>
2022-06-24 13:54:34 +0300

    add

---
+++ 01.Masterchef/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Masterchef        // 100/100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> ingredientsValues = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            Stack<int> freshnessValues = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            SortedDictionary<string, int> result = new SortedDictionary<string, int>();
+
+            while (ingredientsValues.Count>0 && freshnessValues.Count>0)
+            {
+                int currIngredients = ingredientsValues.Peek();
+                int currFreshness=freshnessValues.Peek();
+                if (currIngredients==0)
+                {
+                    ingredientsValues.Dequeue();
+                    continue;
+                }
+                int totalFreshnessLevel = currIngredients * currFreshness;
+                string currDish = GetCurrDish(totalFreshnessLevel);
+                if (currDish.Any())
+                {
+                    ingredientsValues.Dequeue();
+                    freshnessValues.Pop();
+
+                    if (result.ContainsKey(currDish))
+                    {
+                        result[currDish] += 1;
+                    }
+                    else
+                    {
+                        result.Add(currDish, 1);
+                    }
+                }
+                else
+                {
+                    ingredientsValues.Dequeue();
+                    freshnessValues.Pop();
+                    currIngredients += 5;
+                    ingredientsValues.Enqueue(currIngredients);
+                }
+            }
+            if (result.Count==4)
+            {
+                Console.WriteLine("Applause! The judges are fascinated by your dishes!");
+            }
+            else
+            {
+                Console.WriteLine("You were voted off. Better luck next year.");
+            }
+            if (ingredientsValues.Any())
+            {
+                Console.WriteLine($"Ingredients left: {ingredientsValues.Sum()}");
+            }
+            if (result.Any())
+            {
+                foreach (var item in result)
+                {
+                    Console.WriteLine($" # {item.Key} --> {item.Value}");
+                }
+            }           
+        }
+        private static string GetCurrDish(int value)
+        {
+            string currDish = "";                     
+             if (value == 400)
+            {
+                currDish = "Lobster";
+            }
+            else if (value == 300)
+            {
+                currDish = "Chocolate cake";
+            }
+            else if (value == 250)
+            {
+                currDish = "Green salad";
+            }
+            else if (value == 150)
+            {
+                currDish = "Dipping sauce";
+            }
+            return currDish;
+        }
+    }
+}
---
+++ 03.SkiRental - Classes/SkiRental.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkiRental
+{
+    public class SkiRental
+    {
+        private List<Ski> data;
+        public SkiRental(string name, int capacity)
+        {
+            data=new List<Ski>();   
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return data.Count; } }
+
+        //•	Method Add(Ski ski) – adds an entity to the data if there is an empty slot for the Ski.
+        public void Add(Ski ski)
+        {
+            if (Capacity>data.Count)
+            {
+                data.Add(ski);
+            }
+        }
+        public bool Remove(string manufacturer, string model)
+        {
+            Ski ski = data.FirstOrDefault(c => c.Manufacturer == manufacturer && c.Model == model);
+            if (ski == null)
+            {
+                return false;
+            }
+            data.Remove(ski);
+            return true;
+        }
+        public Ski GetNewestSki()
+        {
+            Ski ski = data.OrderByDescending(p => p.Year).FirstOrDefault();
+            return ski;
+        }
+        public Ski GetSki(string manufacturer, string model)
+        {
+            Ski ski = data.FirstOrDefault(c => c.Manufacturer == manufacturer && c.Model == model);
+            return ski;
+        }
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The skis stored in {Name}:");
+            foreach (var item in data)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
